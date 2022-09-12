3ed15ab74367fd01f0d1a52f825d1e159fed6e4d
notEqual99 <phyopyae.za94@gmail.com>
2022-09-12 21:26:59 +0630

    fixing wrong Indefinite Article

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -22,7 +22,7 @@ export default class Projects extends Component {
                   <Card shadow={5}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB5wIiElencE4D-Cv1f3YqouHUFVmmrNzikLVk9ADs244_WBzomw) center / cover'}}>Miracle</CardTitle>
                     <CardText>
-                        Build a ecommerce handmade website.
+                        Build an ecommerce handmade website.
                     </CardText>
                     <CardActions border>
                         <a href="https://github.com/notEqual99/miracle" rel="noopener noreferrer" target="_blank">
@@ -43,7 +43,7 @@ export default class Projects extends Component {
                   <Card shadow={5}>
                     <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB5wIiElencE4D-Cv1f3YqouHUFVmmrNzikLVk9ADs244_WBzomw) center / cover'}}>Vooks</CardTitle>
                     <CardText>
-                        Build a online books library with Laravel.
+                        Build an online books library with Laravel.
                     </CardText>
                     <CardActions border>
                         <a href="https://gitlab.com/phyopyae.za94/vooks" rel="noopener noreferrer" target="_blank">
